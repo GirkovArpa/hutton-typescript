376500bac127707637732b5a02fb4b546c3957e1
GirkovArpa <GirkovArpa@protonmail.com>
2020-08-10 00:38:39 -0500

    make encrypt recursive

--- main.ts
+++ main.ts
@@ -1,22 +1,36 @@
 'use strict';
 
-import { getPermutation } from './getPermutation.ts';
+const abc = 'abcdefghijklmnopqrstuvwxyz';
 
+export const swap = (s: string, i: number, j: number): string => {
+  let a: string[] = [...s];
+  [a[i], a[j]] = [a[j], a[i]];
+  const swapped = a.join('');
+  return swapped;
+}
 
-export const encrypt = (pt: string, pw: string, k: string, perm: string[] = [...getPermutation(k)] ): string => {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const alphabetKeyedConstant = perm.join('').toUpperCase();
-  let output = '';
-  for (let i = 0; i < pt.length; i++) {
-    let shift = alphabet.indexOf(pw[i % pw.length]);
-    shift += alphabet.indexOf(perm[0]);
-    shift += 2;
-    const ptL = pt[i];
-    let ctL_i = (shift + perm.indexOf(ptL)) % alphabet.length;
-    let ctL = perm[ctL_i];
-    output += ctL;
-    let ptL_i = perm.indexOf(ptL);
-    [perm[ctL_i], perm[ptL_i]] = [perm[ptL_i], perm[ctL_i]];
-  }
-  return output;
+const emptyStringIfInSet = (set: Set<string>) => (char: string) => set.has(char) ? '' : char;
+const allLowerCaseChars = /[a-z]/g;
+
+export const permutate = (key: string): string => {
+  const set: Set<string> = new Set(key);
+  const uniques: string = [...set].join('');
+  const alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+  const difference: string = alphabet.replace(allLowerCaseChars, emptyStringIfInSet(set));
+  const permutation: string = uniques.concat(difference);
+  return permutation;
+}
+
+export const encrypt = (pt: string, pw: string, k: string, ct: string = '', perm: string = permutate(k)): string => {
+  if (!pt.length) return ct;
+  const [pwL] = pw;
+  let shift = abc.indexOf(pwL);
+  const [firstCharOfPerm] = perm;
+  shift += abc.indexOf(firstCharOfPerm);
+  shift += 2;
+  const [ptL] = pt;
+  let ctL_i = (shift + perm.indexOf(ptL)) % 26;
+  let ctL = perm[ctL_i];
+  let ptL_i = perm.indexOf(ptL);
+  return encrypt(pt.slice(1), pw.slice(1), k, ct + ctL, swap(perm, ptL_i, ctL_i));
 }
--- test.ts
+++ test.ts
@@ -1,13 +1,16 @@
 'use strict';
 
 import { assertEquals } from 'https://deno.land/std@0.64.0/testing/asserts.ts';
-import { encrypt } from './main.ts';
-import { getPermutation } from './getPermutation.ts';
+import { encrypt, permutate, swap } from './main.ts';
 
-Deno.test('getPermutation', () => {
-  assertEquals(getPermutation('bar'), 'barcdefghijklmnopqstuvwxyz');
+Deno.test('swap', () => {
+  assertEquals(swap('barcdefghijklmnopqstuvwxyz', 8, 16), 'barcdefgpijklmnohqstuvwxyz');
+});
+
+Deno.test('permutate', () => {
+  assertEquals(permutate('bar'), 'barcdefghijklmnopqstuvwxyz');
 });
 
 Deno.test('encrypt', () => {
-  assertEquals(encrypt('helloworld', 'foo', 'bar'), 'pwckfenttc');
+  assertEquals(encrypt('helloworld', 'foo'.repeat(100), 'bar'), 'pwckfenttc');
 });
